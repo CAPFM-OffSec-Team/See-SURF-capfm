98a4dcab3561d3f33d8bcb4e559997c647aebbdd
mehdi.khenissi <mehdi.khenissi@proton.me>
2026-05-20 15:13:17 +0100

    fix: changed the module name in setup.py

--- __main__.py
+++
@@ -1,4 +0,0 @@
-import sys
-import runpy
-
-runpy.run_path(str(__file__).replace("__main__.py", "see-surf.py"), run_name="__main__")
--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='see_surf',
-    version='0.1.0',
+    name="see_surf",
+    version="0.1.0",
     packages=find_packages(),
-    package_data={'see_surf': ['see-surf.py']},
-    install_requires=open('requirements.txt').read().splitlines(),
-    entry_points={
-        'console_scripts': [
-            'see_surf=see_surf:scan',
-        ],
+    include_package_data=True,
+    package_data={
+        "see_surf": ["see_surf.py"]
     },
+    install_requires=open("requirements.txt").read().splitlines(),
+    entry_points={
+        "console_scripts": [
+            "see-surf=see_surf:scan"
+        ]
+    },
+    python_requires=">=3.10",
 )
